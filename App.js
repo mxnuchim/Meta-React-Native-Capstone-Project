9d0ecf9bc33d0f3d2f98e414e396653d4fc378a1
Manuchimso Oliver <manuchimoliver779@gmail.com>
2023-07-21 01:59:11 +0100

    fix: added font loading on startup

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StyleSheet } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import { useMemo, useState, useEffect } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { useMemo, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import Onboarding from "./screens/Onboarding";
-import { AuthContext } from "./store/AuthContext";
-import Loading from "./screens/Loading";
-import Profile from "./screens/Profile";
-import Home from "./screens/Home";
+import Onboarding from './screens/Onboarding';
+import { AuthContext } from './store/AuthContext';
+import Loading from './screens/Loading';
+import Profile from './screens/Profile';
+import Home from './screens/Home';
+import * as Font from 'expo-font';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  useEffect(() => {
+    // Load the custom font asynchronously
+    Font.loadAsync({
+      'Karla-Medium': require('./assets/fonts/Karla-Medium.ttf'),
+    });
+  }, []);
   const [status, setStatus] = useState({
     isLoading: true,
     isOnboardingCompleted: false,
@@ -24,7 +31,7 @@ export default function App() {
     (async () => {
       let profileData = [];
       try {
-        const getProfile = await AsyncStorage.getItem("profile");
+        const getProfile = await AsyncStorage.getItem('profile');
         if (getProfile !== null) {
           profileData = getProfile;
         }
@@ -53,9 +60,9 @@ export default function App() {
       onboard: async (data) => {
         try {
           const jsonValue = JSON.stringify(data);
-          await AsyncStorage.setItem("profile", jsonValue);
+          await AsyncStorage.setItem('profile', jsonValue);
         } catch (e) {
-          console.error("error", e);
+          console.error('error', e);
         }
 
         setStatus({
@@ -69,7 +76,7 @@ export default function App() {
         try {
           await AsyncStorage.clear();
         } catch (e) {
-          console.error("error", e);
+          console.error('error', e);
         }
 
         setStatus({
@@ -81,12 +88,12 @@ export default function App() {
       update: async (data) => {
         try {
           const jsonValue = JSON.stringify(data);
-          await AsyncStorage.setItem("profile", jsonValue);
+          await AsyncStorage.setItem('profile', jsonValue);
         } catch (e) {
-          console.error("error", e);
+          console.error('error', e);
         }
 
-        Alert.alert("Success", "Changes has been successfully saved!");
+        Alert.alert('Success', 'Changes has been successfully saved!');
       },
     }),
     []
